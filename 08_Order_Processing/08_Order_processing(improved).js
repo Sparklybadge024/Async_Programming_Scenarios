72dbfcb0bd340cd79d73c5bd6c23fd06f44c05b8
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-09-20 12:05:43 +0530

    addCategory method Updated

--- 08_Order_Processing/08_Order_processing(improved).js
+++ 08_Order_Processing/08_Order_processing(improved).js
@@ -11,16 +11,16 @@ addBrand(brandName){
 
 addCategory(brandName,categoryName){
     if(this.products.has(brandName)){
-       this.products.get(brandName)[categoryName]={}
+       this.products.get(brandName)[categoryName]=[]
     }else{
         this.addBrand(brandName);
-        this.products.get(brandName)[categoryName]={}
+        this.products.get(brandName)[categoryName]=[]
     }
 }
 
 addProduct(brand,category,productObj){
     if(this.products.has(brand)){
-        if(this.products[brand].has(category)){
+        if(this.products.has(this.products.get(brand)[category])){
             this.products.get(brand)[category][productObj]
         }else{
             this.addCategory(brand,category);
@@ -44,30 +44,42 @@ console.log(s1);
 s1.addCategory("acer","Laptops")
 console.log(s1);
 s1.addProduct("Motorolla","Mobile Phones",{productTitle:"Motorola Moto G85 5G (128, Cobalt Blue, New)",variant:"128GB, Cobalt Blue",price:15949,stock:10,productVisualization:["Image1","Image2","Image3","Video1"],features:[]})
+console.log(s1);
 
 // Test code for mapping
-// let store=new Map();
-// function addBrand(brandName){
-//     if(!store.has(brandName)){
-//         store.set(brandName,{})
-//     }
-//     return store;
-// }
+let store=new Map();
+function addBrand(brandName){
+    if(!store.has(brandName)){
+        store.set(brandName,{})
+    }
+    return store;
+}
 
-// console.log(store);
-// console.log(addBrand("acer"));
-// console.log(store);
-// console.log(addBrand("acer"));
+console.log(store);
+console.log(addBrand("acer"));
+console.log(store);
+console.log(addBrand("acer"));
 
-// function addCategory(brandName,categoryName){
-//     if(store.has(brandName)){
-//         store.get(brandName)[categoryName]={}
-//     }else{
-//         addBrand(brandName);
-//         store.get(brandName)[categoryName]={}
-//     }
-// return store;
-// }
+function addCategory(brandName,categoryName){
+    if(store.has(brandName)){
+        store.get(brandName)[categoryName]={}
+    }else{
+        addBrand(brandName);
+        store.get(brandName)[categoryName]={}
+    }
+return store;
+}
 
-// addCategory("acer","Laptop")
-// console.log(store);
+addCategory("acer","Laptop")
+console.log(store);
+
+
+function addProduct(brandName,categoryName,productObj){
+    if(!store.has(brandName)){
+        addBrand(brandName);
+    }else if(!store.get(brandName)[categoryName]){
+        addCategory(brandName,categoryName);
+    }else{
+        store.get(brandName)[categoryName][productObj];
+    }
+}

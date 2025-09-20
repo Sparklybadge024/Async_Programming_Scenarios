5b4a6e15d9a0fc32318bf1d89cede420ff405cdb
Rishabh Sundli <rishabhsundli024@gmail.com>
2025-09-20 14:08:04 +0530

    Whole code has been polished

--- 08_Order_Processing/08_Order_processing(improved).js
+++ 08_Order_Processing/08_Order_processing(improved).js
@@ -7,31 +7,32 @@ addBrand(brandName){
     if(!this.products.has(brandName)){
         this.products.set(brandName,{});
     }
+    return this.products.get(brandName)
 }
 
 addCategory(brandName,categoryName){
-    if(this.products.has(brandName)){
-       this.products.get(brandName)[categoryName]=[]
+    if(!this.products.has(brandName)){
+       this.addBrand(brandName);
+       this.products.get(brandName)[categoryName]=[];
     }else{
-        this.addBrand(brandName);
-        this.products.get(brandName)[categoryName]=[]
+        this.products.get(brandName)[categoryName]=[];
     }
+    return this.products.get(brandName)
 }
 
 addProduct(brand,category,productObj){
-    if(this.products.has(brand)){
-        if(this.products.has(this.products.get(brand)[category])){
-            this.products.get(brand)[category][productObj]
-        }else{
-            this.addCategory(brand,category);
-            this.products.get(brand)[category][productObj]
-        }
-    }else{
+    if(!this.products.has(brand)){
         this.addBrand(brand);
         this.addCategory(brand,category);
-        this.addProduct(brand,category,productObj)
     }
+    if(!this.products.get(brand)[category]){
+        this.addCategory(brand,category);
+    }
+    this.products.get(brand)[category].push(productObj)
+    
+    return this.products.get(brand)[category]
 }
+
 }
 // testing the actual code
 let s1=new Store;
@@ -43,43 +44,48 @@ s1.addBrand("Motorolla")
 console.log(s1);
 s1.addCategory("acer","Laptops")
 console.log(s1);
-s1.addProduct("Motorolla","Mobile Phones",{productTitle:"Motorola Moto G85 5G (128, Cobalt Blue, New)",variant:"128GB, Cobalt Blue",price:15949,stock:10,productVisualization:["Image1","Image2","Image3","Video1"],features:[]})
-console.log(s1);
+console.log(s1.addProduct("Motorolla","Mobile Phones",{productTitle:"Motorola Moto G85 5G (128, Cobalt Blue, New)",variant:"128GB, Cobalt Blue",price:15949,Stock:10,productVisualization:["Image1","Image2","Image3","Video1"],features:[]}));
+
+
+
+
+
+
 
 // Test code for mapping
-let store=new Map();
-function addBrand(brandName){
-    if(!store.has(brandName)){
-        store.set(brandName,{})
-    }
-    return store;
-}
+// let store=new Map();
+// function addBrand(brandName){
+//     if(!store.has(brandName)){
+//         store.set(brandName,{})
+//     }
+//     return store;
+// }
 
-console.log(store);
-console.log(addBrand("acer"));
-console.log(store);
-console.log(addBrand("acer"));
+// console.log(store);
+// console.log(addBrand("acer"));
+// console.log(store);
+// console.log(addBrand("acer"));
 
-function addCategory(brandName,categoryName){
-    if(store.has(brandName)){
-        store.get(brandName)[categoryName]={}
-    }else{
-        addBrand(brandName);
-        store.get(brandName)[categoryName]={}
-    }
-return store;
-}
+// function addCategory(brandName,categoryName){
+//     if(store.has(brandName)){
+//         store.get(brandName)[categoryName]={}
+//     }else{
+//         addBrand(brandName);
+//         store.get(brandName)[categoryName]={}
+//     }
+// return store;
+// }
 
-addCategory("acer","Laptop")
-console.log(store);
+// addCategory("acer","Laptop")
+// console.log(store);
 
 
-function addProduct(brandName,categoryName,productObj){
-    if(!store.has(brandName)){
-        addBrand(brandName);
-    }else if(!store.get(brandName)[categoryName]){
-        addCategory(brandName,categoryName);
-    }else{
-        store.get(brandName)[categoryName][productObj];
-    }
-}
+// function addProduct(brandName,categoryName,productObj){
+//     if(!store.has(brandName)){
+//         addBrand(brandName);
+//     }else if(!store.get(brandName)[categoryName]){
+//         addCategory(brandName,categoryName);
+//     }else{
+//         store.get(brandName)[categoryName][productObj];
+//     }
+// }
